6fd12bea75115fa2ea10ce11bc8662ee0925c376
AleksaSt <alexastojcevic@yahoo.com>
2021-10-03 19:34:22 +0200

    conflict resolved

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,7 @@ import FooterCSS from "../css/Footer.module.css"
 const Footer = () => {
   return (
     <div className={FooterCSS.mainFooter}>
-<<<<<<< HEAD
-      {/* <h1>blabla</h1> */}
-=======
       <h1>footer</h1>
->>>>>>> 8791ca59e519e4575be3d626c68129d2f5c79768
     </div>
   )
 }
